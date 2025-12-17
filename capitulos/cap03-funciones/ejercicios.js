1984cb885b0121d4ced394e6ddd2e039851a7f06
AlvaroAlcocer <alvaroalcocer25@gmail.com>
2025-12-17 15:54:34 -0400

    Ejercicios 1 y 2 Funciones

--- capitulos/cap03-funciones/ejercicios.js
+++ capitulos/cap03-funciones/ejercicios.js
@@ -3,8 +3,13 @@
 // Crea una función llamada 'saludar' que reciba un nombre como parámetro
 // y devuelva un texto que diga "Hola, [nombre]!".
 // Prueba la función con varios nombres.
-
-
+function saludar(nombre) {
+    return "hola, " + nombre;
+}
+console.log(saludar("Alex"));
+console.log(saludar("Mateo"));
+console.log(saludar("Maria"));
+console.log(saludar("Andres"));
 
 
 // -----------------------------------------------------------------------------
@@ -12,8 +17,11 @@
 // Escribe una función llamada 'cuadrado' que reciba un número
 // y devuelva ese número multiplicado por sí mismo.
 // Ejemplo: cuadrado(5) debería devolver 25.
-
-
+function cuadrado(numero) {
+    return numero * numero
+}
+console.log(cuadrado(4))
+console.log(cuadrado(5))
 
 
 // -----------------------------------------------------------------------------
@@ -21,7 +29,11 @@
 // Crea una función 'areaRectangulo' que reciba 'base' y 'altura'.
 // Debe devolver el área (base * altura).
 // Si no se le pasa altura, debe asumir que es un cuadrado (altura = base). (Pista: usa parámetros por defecto).
-
+function areaRectangulo(base, altura = base) {
+    return base * altura;
+}
+console.log(areaRectangulo(2, 2));
+console.log(areaRectangulo(2));
 
 
 
@@ -34,8 +46,8 @@ function multiplicar(a, b) {
     return a * b;
 }
 */
-
-
+const multiplicar = (a, b) => a * b;
+console.log("multiplicar:", multiplicar(4, 2));
 
 
 
@@ -48,7 +60,15 @@ function multiplicar(a, b) {
 // min(0, 10) -> 0
 // min(0, -10) -> -10
 
-
+function min(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+console.log("El minimo es:", min(5, 2));
+console.log("El minimo es:", min(5, -6));
 
 
 // -----------------------------------------------------------------------------
@@ -61,7 +81,25 @@ function multiplicar(a, b) {
 // - Para cualquier otro número N, su paridad es la misma que N - 2.
 // Define una función recursiva 'esPar' que corresponda a esta descripción.
 // La función debe aceptar un solo parámetro (un número entero, positivo) y devolver un Booleano.
+function esPar(n) {
+    if (n == 0) {
+        return true;
+    } else if (n == 1) {
+        return false;
+    } else {
+        return esPar(n - 2);
+    }
+}
+console.log("esPar:", esPar(2));
+console.log("esPar:", esPar(3));
 
+function esPar(n) {
+    if (n === 0) return true;
+    if (n === 1) return false;
+    return esPar(n - 2);
+}
+console.log(esPar(55));
+console.log(esPar(60));
 
 
 // -----------------------------------------------------------------------------
@@ -70,6 +108,20 @@ function multiplicar(a, b) {
 // y un carácter como segundo argumento, y devuelva el número de veces que ese carácter
 // aparece en el string. Usa un bucle for dentro de la función.
 
+function contarBase(texto, caracter) {
+    let contador = 0;
+
+    for (let i = 0; i < texto.length; i++) {
+        if (texto[i] === caracter) {
+            contador++;
+        }
+    }
+
+    return contador;
+}
+
+console.log(contarBase("BBBBC", "B"));
+console.log(contarBase("banana", "a"));
 
 
 
---
+++ capitulos/cap03-funciones/ejercicios2.js
@@ -0,0 +1,16 @@
+//Jochicicios
+//1.Factorial con recursividad
+//2.Potencia con recursividad
+//3.Convertir texto a un array de caracteres
+//4. Generador de numeros aleatorios por rango
+//5. Conjetura de Collatz (for y recursiva para mayor reto)
+//6. Serie de fibonacci (for y recursiva para mayor reto)
+//7. Numero a binario
+//8. Binario a numero
+//9. Numero a hexadecimal
+//10. Conversor de segundos a horas y viceversa
+//12. Teorema de pitagoras
+//13. Imprima digitos de numero (usando logaritmos o operaciones matematicas)
+//14. Formula general
+//15. Multiplicacion y division (solo suma o resta)
+//16. Banco calcule el cambio de una compra, solo billetes de 100, 50,20,10,5,2,1
